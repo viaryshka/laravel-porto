158b96f0bdb15573bd8645bccbc3d201a7f911c8
Anastas Mironov <ast@l80.ru>
2024-04-09 18:46:13 +0700

    feat: remove RouteServiceProvider.php

--- src/Loaders/AutoLoaderTrait.php
+++ src/Loaders/AutoLoaderTrait.php
@@ -11,20 +11,33 @@ trait AutoLoaderTrait
     use LocalizationLoaderTrait;
     use MigrationsLoaderTrait;
     use ProvidersLoaderTrait;
+    use RoutesLoaderTrait;
     use ViewsLoaderTrait;
+
+    protected ?string $containerPath = null;
+
+    public function getContainerPath(): string
+    {
+        if (is_null($this->containerPath)) {
+            return $this->containerPath = realpath(dirname((new \ReflectionClass($this))->getFileName()).'/..');
+        }
+
+        return $this->containerPath;
+    }
 
     public function registerContainer(): void
     {
         $this->loadServiceProviders();
-        $this->loadConfigsFromContainers($this->containerPath);
-        $this->loadLocalsFromContainers($this->containerPath);
+        $this->loadConfigsFromContainers($this->getContainerPath());
+        $this->loadLocalsFromContainers($this->getContainerPath());
     }
 
     public function bootContainer(): void
     {
-        $this->loadMigrationsFromContainers($this->containerPath);
-        $this->loadViewsFromContainers($this->containerPath);
-        $this->loadHelpersFromContainers($this->containerPath);
-        $this->loadCommandsFromContainers($this->containerPath);
+        $this->runRoutesAutoLoader($this->getContainerPath());
+        $this->loadMigrationsFromContainers($this->getContainerPath());
+        $this->loadViewsFromContainers($this->getContainerPath());
+        $this->loadHelpersFromContainers($this->getContainerPath());
+        $this->loadCommandsFromContainers($this->getContainerPath());
     }
 }
